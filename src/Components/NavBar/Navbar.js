0bf9fe9c3fa09055c342985dc3169e2978431be2
1Sarry <saronmeles4@gmail.com>
2023-08-12 11:41:48 +0300

    Navbar scroll issue fixed again

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -6,16 +6,26 @@ import avatar from "../../Images/Netflix-avatar.png";
 const Navbar = () => {
   const [show, handleShow] = useState(false);
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    // window.addEventListener("scroll", () => {
+    //   if (window.scrollY >= 100) {
+    //     handleShow(true);
+    //   } else {
+    //     handleShow(false);
+    //   }
+    // });
+    // return () => {
+    //   window.removeEventListener("scroll", null);
+    // };
+
+   const showNav = () => {
       if (window.scrollY >= 100) {
         handleShow(true);
       } else {
         handleShow(false);
       }
-    });
-    return () => {
-      window.removeEventListener("scroll", null);
-    };
+   }
+     showNav()
+     console.log(show)
   }, [window.scrollY]);
 
   return (
